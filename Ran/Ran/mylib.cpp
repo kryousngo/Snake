6632c1eb596477890a4f1d7bbefa262d15cf82e8
DontAsk117 <149929318+DontAsk117@users.noreply.github.com>
2023-11-15 23:41:29 +0700

    Update mylib.cpp

--- Ran/Ran/mylib.cpp
+++ Ran/Ran/mylib.cpp
@@ -41,47 +41,6 @@ void clrscr()
 
 
 
-#include "mylib.h"
-
-// Lấy nút bàn phím do người dùng bấm
-// Trả về: Mã của phím
-int inputKey()
-{
-	if (_kbhit())
-	{
-		int key = _getch();
-
-		if (key == 224)
-		{
-			key = _getch();
-			return key + 1000;
-		}
-		return key;
-	}
-	else
-	{
-		return KEY_NONE;
-	}
-	return KEY_NONE;
-}
-
-// Xóa màn hình
-void clrscr()
-{
-	CONSOLE_SCREEN_BUFFER_INFO	csbiInfo;
-	HANDLE	hConsoleOut;
-	COORD	Home = { 0,0 };
-	DWORD	dummy;
-
-	hConsoleOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	GetConsoleScreenBufferInfo(hConsoleOut, &csbiInfo);
-
-	FillConsoleOutputCharacter(hConsoleOut, ' ', csbiInfo.dwSize.X * csbiInfo.dwSize.Y, Home, &dummy);
-	csbiInfo.dwCursorPosition.X = 0;
-	csbiInfo.dwCursorPosition.Y = 0;
-	SetConsoleCursorPosition(hConsoleOut, csbiInfo.dwCursorPosition);
-}
-
 // Di chuyển con trỏ console đến vị trí có tọa độ (x, y)
 void gotoXY(int x, int y)
 {
