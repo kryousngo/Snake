92d23ee18e8aaebcf144935a0cf601efbaa5703b
Phan Minh Quang <150677490+MinhquangCN2023@users.noreply.github.com>
2023-11-15 14:50:40 +0700

    V1_mylib.cpp
    Quang

--- Ran/Ran/mylib.cpp
+++ Ran/Ran/mylib.cpp
@@ -1,1 +1,41 @@
+#include "mylib.h"
+
+// Lấy nút bàn phím do người dùng bấm
+// Trả về: Mã của phím
+int inputKey()
+{
+	if (_kbhit())
+	{
+		int key = _getch();
+
+		if (key == 224)
+		{
+			key = _getch();
+			return key + 1000;
+		}
+		return key;
+	}
+	else
+	{
+		return KEY_NONE;
+	}
+	return KEY_NONE;
+}
+
+// Xóa màn hình
+void clrscr()
+{
+	CONSOLE_SCREEN_BUFFER_INFO	csbiInfo;
+	HANDLE	hConsoleOut;
+	COORD	Home = { 0,0 };
+	DWORD	dummy;
+
+	hConsoleOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	GetConsoleScreenBufferInfo(hConsoleOut, &csbiInfo);
+
+	FillConsoleOutputCharacter(hConsoleOut, ' ', csbiInfo.dwSize.X * csbiInfo.dwSize.Y, Home, &dummy);
+	csbiInfo.dwCursorPosition.X = 0;
+	csbiInfo.dwCursorPosition.Y = 0;
+	SetConsoleCursorPosition(hConsoleOut, csbiInfo.dwCursorPosition);
+}
 
